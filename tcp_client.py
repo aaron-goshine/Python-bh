0bc0d6689ccb7db67bb9d1ed61a449ed77f8e3dd
Goshine <greenlig@gmail.com>
2016-07-25 06:49:09 +0100

    feat(Implemented TCP Sever): Implemented basic tcp server and added gitignore and more

--- tcp_client.py
+++ tcp_client.py
@@ -1,16 +1,20 @@
 import socket
 
-target_host = "127.0.0.1"
+target_host = "www.goshine-dev.co.uk"
 target_port = 80
 
 # crate a socket object
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client
+client.connect((target_host, target_port))
 
 # send some data
-client.sendto("AARON SAMARO GOSHINE", (target_host, target_port))
+client.send("GET / HTTP/1.1\r\nHost: goshine-dev.co.uk\r\n\r\n")
 
 # receive some data
-data, addr = client.recvfrom(4096)
 
-print(data)
+response = client.recv(4096)
+
+print(response)
 
